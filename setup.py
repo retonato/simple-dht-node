8fd5a56b65d529905890475faec1f479c9e49bee
retonato <retonato@protonmail.com>
2021-01-22 17:45:46 +0100

    Add description and url to the package

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 """Setup file, used for building a Python package."""
 from setuptools import setup
 
+with open("README.md", "r") as desc_file:
+    long_description = desc_file.read()
+
 setup(
     install_requires=["cachetools", "modern-bencode"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     name="simple-dht-node",
     packages=["dht_node"],
     package_data={"dht_node": ["py.typed"]},
     python_requires=">=3.7",
+    url="https://github.com/retonato/simple-dht-node",
     version="1.0.0",
 )
